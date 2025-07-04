ce74e2fa38c47acaa825dbeef585fa1d6f74522b
Mohsen Rahmanikivi <89780859+mohsenrahmanikivi@users.noreply.github.com>
2025-07-04 13:00:03 +0200

    Update default.launch.py

--- launch/default.launch.py
+++ launch/default.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_directory
 
